d378657aaaa6ebd629f395403dddd62628a9773a
Thomas Kristensen <thomas.kristensen@uswitch.com>
2014-06-26 14:38:16 +0100

    Add ClojureScript build through cljx.

--- project.clj
+++ project.clj
@@ -2,4 +2,20 @@
   :description "A predicate dispatch library for Clojure."
   :url "http://github.com/tgk/kaleidoscope"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [com.keminglabs/cljx "0.3.0"]]
+  :jar-exclusions [#"\.cljx"]
+  :cljx {:builds [{:source-paths ["src/"]
+                   :output-path "target/generated"
+                   :rules :clj}
+
+                  {:source-paths ["src/"]
+                   :output-path "target/generated"
+                   :rules :cljs}]}
+  :hooks [cljx.hooks]
+  :source-paths ["src"  "target/generated"]
+  :cljsbuild {:builds [{:source-paths ["target/generated"]
+                        :compiler {:output-to "resources/public/build/deps.js"
+                                   :output-dir "resources/public/build"
+                                   :optimizations :none}}]})
