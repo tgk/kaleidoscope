0bd31ada3981497b1e081796d653a18701acacd6
Thomas Kristensen <thomas.kristensen@uswitch.com>
2014-06-26 14:42:53 +0100

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kaleidoscope "0.1.0"
+(defproject kaleidoscope "0.1.1-SNAPSHOT"
   :description "A predicate dispatch library for Clojure."
   :url "http://github.com/tgk/kaleidoscope"
   :license {:name "Eclipse Public License"
